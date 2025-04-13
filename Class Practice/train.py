1be4f37ae59026486874cbe533343004ae6b401b
aidan.wong16 <aidan.wong16@education.nsw.gov.au>
2025-04-13 21:08:34 +1000

    added code to move function in train class

--- Class Practice/train.py
+++ Class Practice/train.py
@@ -2,16 +2,16 @@ class Train:
 
     def __init__(self, current_position):
         self.current_position = current_position
-        print(self.current_position)
 
     def move(self, additional_distance):
-        pass
+        self.current_position = self.current_position + additional_distance
 
     def get_position(self):
         pass
 
-    position = 10
 
-#Write a piece of coding where it starts at 10m, and it moves by 5m, and get the position after.
+# Write a piece of coding where it starts at 10m, and it moves by 5m, and get the position after.
 
 train = Train(10)
+train.move(5)
+print(train.current_position)
