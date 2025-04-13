86b8f898b37f43364a97f19f5035f623c2ed9855
aidan.wong16 <aidan.wong16@education.nsw.gov.au>
2025-04-13 20:34:07 +1000

    added to __init__ statement in class

--- Class Practice/train.py
+++ Class Practice/train.py
@@ -2,6 +2,7 @@ class Train:
 
     def __init__(self, current_position):
         self.current_position = current_position
+        print(self.current_position)
 
     def move(self, additional_distance):
         pass
@@ -9,4 +10,8 @@ class Train:
     def get_position(self):
         pass
 
+    position = 10
+
 #Write a piece of coding where it starts at 10m, and it moves by 5m, and get the position after.
+
+train = Train(10)
