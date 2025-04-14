8c164da1b7d923d9adaee59b3b5f194a15df1e5a
aidan.wong16 <aidan.wong16@education.nsw.gov.au>
2025-04-14 16:13:33 +1000

    added number to word conversion

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ def main():
     calculator = Calculator()
     calculator.get_all_number()
     calculator.get_operator()
-    print(calculator.all_numbers)
-    print(calculator.operator)
+    
     print(
         f"""------------------------------------------------
-        {calculator.get_result()} """
+    {calculator.get_result()} """
     )
+
+
 
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -34,7 +34,7 @@ class Calculator:
 
         while True:
             concat_string = ", ".join(self.accepted_operators)
-            operator = input(f"enter an operator({concat_string}):")
+            operator = input(f"enter an operator({concat_string}): ")
             if operator in self.accepted_operators:
                 self.operator = operator
                 break
@@ -57,6 +57,6 @@ class Calculator:
 
             result = operator_map[self.operator]
 
-            print(result)
+            #print(result)
 
         return result
