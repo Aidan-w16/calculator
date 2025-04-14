a868b373aa590f61f97ddf14d112a1dd0ccfeb59
aidan.wong16 <aidan.wong16@education.nsw.gov.au>
2025-04-14 13:57:17 +1000

    created a list for all the numbers and made an input statement for the amount of numbers

--- model.py
+++ model.py
@@ -2,9 +2,10 @@
 class Calculator:
 
     def __init__(self):
-        self.first_number = 0
-        self.second_number = 0
+        self.number_required = None
+        self.all_numbers = []
         self.operator = None
+        self.accepted_operators = ["+", "-", "*", "X", "/"]
 
     def _get_number(self, display_text):
         while True:
@@ -16,17 +17,18 @@ class Calculator:
                 print("Enter Numbers ONLY.")
 
     def get_number(self):
-        self.first_number = self._get_number("Ënter the first number.")
-        self.second_number = self._get_number("Ënter the second number.")
+        self.number_required = self.get_number("How many numbers do you need to calculate?")
+        # self.first_number = self._get_number("Ënter the first number.")
+        # self.second_number = self._get_number("Ënter the second number.")
 
     def get_operator(self):
 
-        accepted_list = ["+", "-", "*", "X", "/"]
+        
 
         while True:
-            concat_string = ', '.join(accepted_list)
+            concat_string = ', '.join(self.accepted_operators)
             operator = input(f"enter an operator({concat_string}):")
-            if operator in accepted_list:
+            if operator in self.accepted_operators:
                 self.operator = operator
                 break
             else:
